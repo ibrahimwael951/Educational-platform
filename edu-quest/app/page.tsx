e28a7864a761fa1e515acb53a5f33b6f2582356c
rawan ayman <rawanrooney229@gmail.com>
2025-03-14 00:36:54 +0200

    broswe by categories section

--- edu-quest/app/page.tsx
+++ edu-quest/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "./components/home/hero/heroSection";
+import CategoriesSection from "./components/home/categories/categoriesSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <CategoriesSection />
     </main>
   );
 }
--- edu-quest/tailwind.config.js
+++ edu-quest/tailwind.config.js
@@ -1,27 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         white: "#FFF",
         black: "#000",
-
-        // Black Shades
-        black: {
-          6: "#0F0F0F",
-          8: "#141414",
-          10: "#1A1A1A",
-          12: "#1F1F1F",
-          15: "#262626",
-          20: "#333333",
-          25: "#404040",
-          30: "#4C4C4C",
-        },
       },
     },
   },
