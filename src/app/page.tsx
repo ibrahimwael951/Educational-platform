5f53a0b19ebc962ad789fc8096e18410298b6325
rawan ayman <rawanrooney229@gmail.com>
2025-03-15 04:18:45 +0200

    skills and why choose us sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import CategoriesSection from "@/components/home/categoriesSection";
 import CoursesSection from "@/components/home/CoursesSection";
 import AboutUsSection from "@/components/home/aboutUsSection";
 import NewSessionSection from "@/components/home/newSessionSection";
+import WhyChooseUsSection from "@/components/home/chooseUsSection";
+import SkillsSection from "@/components/home/skillsSection";
 
 
 export default function Home() {
@@ -13,6 +15,8 @@ export default function Home() {
       <CoursesSection />
       <AboutUsSection />
       <NewSessionSection />
+      <WhyChooseUsSection />
+      <SkillsSection />
     </main>
   );
 }
---
+++ src/components/home/skillsSection.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+const skills = [
+  { img: "/home/s1.png", number: "3K+", text: "Successfully Trained" },
+  { img: "/home/s2.png", number: "15K+", text: "Classes Completed" },
+  { img: "/home/s3.png", number: "97K+", text: "Satisfaction Rate" },
+  { img: "/home/s4.png", number: "102K+", text: "Students Community" },
+];
+
+const SkillsSection = () => {
+  return (
+    <section className="container flex mx-auto bg-cover bg-center rounded-[200px] md:rounded-[50px] justify-center items-center text-center"
+      style={{ backgroundImage: "url('/home/skill background.png')" }}
+    >
+      <div className="flex gap-20 justify-between items-center text-center ">
+        {skills.map((skill, index) => (
+          <div key={index} className="flex md:flex-row items-center text-left bg-white px-[40px] py-[20px] rounded-xl shadow-lg">
+            <Image src={skill.img} alt="icon" width={50} height={50} className="w-12 h-12" />
+            <div className="md:ml-3 text-center md:text-left mt-2 md:mt-0">
+              <h6 className="text-[#0E2A46] font-bold text-3xl text-left">{skill.number}</h6>
+              <p className="text-[#0E2A46] text-sm">{skill.text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default SkillsSection;
