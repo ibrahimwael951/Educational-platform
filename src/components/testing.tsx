a0d2c0c4b0882890584b3613dbcc03f5d3173374
ibrahimwael951 <ibrahimweal809@gmail.com>
2025-03-23 12:34:45 +0200

    add about us page

--- src/components/testing.tsx
+++ src/components/testing.tsx
@@ -1,19 +1,16 @@
-import React from 'react'
+import React from "react";
 
 //components
 import { ModeToggle } from "@/components/themeToggole";
-import LocaleSwitcher from "@/components/langToggle"
-import { useTranslations } from 'next-intl';
+import LocaleSwitcher from "@/components/langToggle";
 
 const Testing = () => {
-    const t = useTranslations('HomePage');
   return (
-    <div className='fixed top-20  left-10 flex flex-col gap-2 bg-white dark:bg-black z-50 p-10 rounded-lg '>
-        <h1 className='text-white'>{t('title')}</h1>
-      <ModeToggle/>
-      <LocaleSwitcher/>
+    <div className="fixed top-20  left-10 flex flex-col gap-2 bg-white dark:bg-black z-50 p-10 rounded-lg ">
+      <ModeToggle />
+      <LocaleSwitcher />
     </div>
-  )
-}
+  );
+};
 
-export default Testing
+export default Testing;
